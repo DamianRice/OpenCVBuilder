c74e3e001548cb1adc05ac99758f91bc800f7fa6
DamianRice <mi007123@163.com>
2025-06-20 16:13:28 +0800

    change: 更新了Linux workflow和编译脚本

--- build-opencv4-linux.sh
+++ build-opencv4-linux.sh
@@ -5,9 +5,29 @@
 # ./build-opencv4-linux.sh -n arm64
 # ./build-opencv4-linux.sh -n ppc64le
 
+_list_of_build_type="Debug Release"
+_list_of_arch_type="386 amd64 arm arm64 ppc64le"
+
 HOST_OS=$(uname -s)
 NUM_THREADS=1
 BUILD_TYPE=Release
+JAVA_FLAG=""
+TARGET_ARCH=""
+
+function exists_in_list() {
+    LIST=$1
+    DELIMITER=$2
+    VALUE=$3
+    [[ "$LIST" =~ ($DELIMITER|^)$VALUE($DELIMITER|$) ]]
+}
+
+function usage(){
+    echo -e "-n -c are required, -j is to build OpenCV Java
+    -n: Arch [386 amd64 arm arm64 ppc64le]
+    -c: Build type [Debug, Release]
+    -j: Enable Build OpenCV Java
+    "
+}
 
 if [ "$HOST_OS" == "Linux" ]; then
     NUM_THREADS=$(nproc)
@@ -16,31 +36,41 @@ else
     exit 0
 fi
 
-JAVA_FLAG=""
-TARGET_ARCH=""
-
-while getopts "n:j" arg; do
+while getopts ":n:c:j" arg; do
     case $arg in
     n)
-        echo "n(TARGET_ARCH):$OPTARG"
-        TARGET_ARCH="$OPTARG"
+        if exists_in_list "$_list_of_arch_type" " " "$OPTARG"; then
+          echo "n(TARGET_ARCH):$OPTARG"
+          TARGET_ARCH="$OPTARG"
+        else
+          echo "n's arg must be 386 amd64 arm arm64 or ppc64le, now is ${OPTARG}"
+        fi
+
+        if [ -z "$TARGET_ARCH" ]; then
+            echo -e "empty TARGET_ARCH."
+            echo -e "usage: ./build-opencv3-linux.sh -n amd64"
+            exit 1
+        fi
+        ;;
+    c)
+        if exists_in_list "$_list_of_build_type" " " "$OPTARG"; then
+          echo "c's arg:$OPTARG"
+          BUILD_TYPE=$OPTARG
+        else
+          echo "c's arg must be Debug,Release,MinSizeRel or RelWithDebInfo, now is $OPTARG"
+        fi
         ;;
     j)
         echo "j's arg:$OPTARG"
         JAVA_FLAG="-DBUILD_FAT_JAVA_LIB=ON -DBUILD_JAVA=ON -DBUILD_opencv_java=ON -DBUILD_opencv_flann=ON"
         ;;
     ?)
-        echo -e "unkonw argument."
-        exit 1
+        echo -e "Unknown argument." usage && exit 1;
         ;;
     esac
 done
 
-if [ -z "$TARGET_ARCH" ]; then
-    echo -e "empty TARGET_ARCH."
-    echo -e "usage: ./build-opencv3-linux.sh -n amd64"
-    exit 1
-fi
+
 
 # get gcc version
 export _compiler=$(which gcc)
@@ -59,16 +89,21 @@ else
     DISABLE_OPTION=""
 fi
 
-BUILD_OUTPUT_PATH="build-$BUILD_TYPE-$TARGET_ARCH"
-BUILD_INSTALL_PATH="$BUILD_OUTPUT_PATH/install"
+BUILD_OUTPUT_PATH="build-Linux-$BUILD_TYPE-$TARGET_ARCH"
+BUILD_INSTALL_PATH="$BUILD_OUTPUT_PATH/install/$BUILD_TYPE"
+OPENCV_CONTRIB_DIR=realpath "../opencv_contrib/modules"
+
 mkdir -p "$BUILD_OUTPUT_PATH"
 
-cmake --compile-no-warning-as-error \
-    -B"$BUILD_OUTPUT_PATH" \
-    -DCMAKE_INSTALL_PREFIX="$BUILD_INSTALL_PATH" \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE \
-    $(cat opencv4_cmake_options.txt) \
-    $DISABLE_OPTION \
+cmake --compile-no-warning-as-error                 \
+    -B"$BUILD_OUTPUT_PATH"                          \
+    -DCMAKE_INSTALL_PREFIX="$BUILD_INSTALL_PATH"    \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE                  \
+    -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE         \
+    -DOPENCV_EXTRA_MODULES_PATH=$OPENCV_CONTRIB_DIR \
+    $(cat opencv4_cmake_options.txt)                \
+    $DISABLE_OPTION                                 \
     $JAVA_FLAG
 
-cmake --build $BUILD_OUTPUT_PATH --config $BUILD_TYPE --parallel $NUM_THREADS --target install
+cmake --build "$BUILD_OUTPUT_PATH" --config $BUILD_TYPE --parallel $NUM_THREADS
+cmake --install "$BUILD_OUTPUT_PATH" --config $BUILD_TYPE --prefix "$BUILD_INSTALL_PATH"
